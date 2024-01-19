f27f4e98d51c65e4b8b7c6c0f2a3e4fb736ff943
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2024-01-19 03:24:53 -0500

    feat: working version directly passing through to client cli

--- src/cli.ts
+++ src/cli.ts
@@ -1,27 +1,5 @@
 #!/usr/bin/env node
 
-import { create } from 'create-create-app';
-import { resolve } from 'path';
+import { init } from "@axiom-crypto/client/cli/init";
 
-const templateRoot = resolve(__dirname, '..', 'templates');
-
-const caveat = `
-This is a caveat!
-You can change this in \`src/cli.ts\`.
-`;
-
-// See https://github.com/uetchy/create-create-app/blob/master/README.md for other options.
-
-create('create-axiom-client', {
-  templateRoot,
-  extra: {
-    architecture: {
-      type: 'list',
-      describe: 'choose your fave os',
-      choices: ['macOS', 'Windows', 'Linux'],
-      prompt: 'if-no-arg',
-    },
-  },
-  after: ({ answers }) => console.log(`Ok you chose ${answers.architecture}.`),
-  caveat,
-});
+init({});
