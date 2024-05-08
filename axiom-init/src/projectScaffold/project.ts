d27a9b8c2b5cf7e830c460adc9277d9a93da9199
Yu Jiang Tham <ytham@users.noreply.github.com>
2024-05-08 15:24:39 -0400

    Handle git history/submodules (#9)
    * Update remove git history and install axiom-std dynamically
    
    * Add github workflow
    
    * Add build
    
    * Update working dirs
    
    * Update workflow
    
    * Update call
    
    * Use provider_uri backup
    
    * Run script
    
    * Update orering
    
    * Update env vars
    
    * Update workflow
    
    * Fix workflowS
    
    * Update workflow
    
    * Update yarn runner
    
    * Commit pnpm-lock
    
    * Fix chainId workflow
    
    * Update forge test
    
    * Commit changes, update workflow
    
    * Workflow logging
    
    * Update github username
    
    * Tweak ci params
    
    * Update exec

--- axiom-init/src/projectScaffold/project.ts
+++ axiom-init/src/projectScaffold/project.ts
@@ -1,58 +1,16 @@
-import chalk from 'chalk';
 import { ProjectScaffoldManager } from './projectScaffoldManager';
+import { scaffoldNext } from './nextjs';
+import { scaffoldScript } from './script';
+import { scaffoldForge } from './forge';
 
-export const scaffoldProject = async (sm: ProjectScaffoldManager, appScaffold: string) => {
-  const startingDir = process.cwd();
-
-  // Create folder if it doesn't exist
-  if (sm.exists(".", `Directory ${chalk.bold(sm.basePath)} exists?`)) {
-    throw new Error("Please select an empty directory");
-  }
-  sm.mkdir(".", `  - Create directory ${chalk.bold(sm.basePath)}`);
-
-  // Move to base path
-  process.chdir(sm.basePath);
-
-  // Clone quickstart template
-  const tempDir = `.axiom-temp-${Date.now()}`;
-  console.log("Fetching Axiom quickstart template...");
-  await sm.exec(`git clone --depth 1 https://github.com/axiom-crypto/axiom-quickstart.git ${tempDir}`, "Clone Axiom quickstart template");
-  sm.cp(`${tempDir}/.`, ".", `  - Copy axiom-quickstart files to ${chalk.bold(sm.basePath)}`);
-
+export const scaffoldProject = async (sm: ProjectScaffoldManager, appScaffold: string) => {  
   if (appScaffold === "nextjs") {
-    const appPath = "app";
-
-    // Delete app folder
-    await sm.rm("app", `  - Remove cloned quickstart scaffold's ${chalk.bold("app")} folder`);
-
-    // Update root package.json to remove start script
-    const rootPackageJson = sm.readFile("package.json");
-    if (rootPackageJson) {
-      const packageJsonObj = JSON.parse(rootPackageJson);
-      if (packageJsonObj.scripts && packageJsonObj.scripts.start) {
-        delete packageJsonObj.scripts.start;
-        sm.writeFile("package.json", JSON.stringify(packageJsonObj, null, 2));
-      }
-    }
-
-    // Clone next.js scaffold to app folder
-    console.log("Fetching Axiom Next.js scaffold...");
-    const nextjsTempDir = `.axiom-temp-nextjs-${Date.now()}`; 
-    await sm.exec(`git clone --depth 1 https://github.com/axiom-crypto/axiom-scaffold-nextjs.git ${nextjsTempDir}`, "Clone Axiom Next.js scaffold");
-    sm.cp(`${nextjsTempDir}`, appPath, `  - Copy Next.js scaffold files to ${chalk.bold(sm.basePath)}`);
-
-    await sm.exec(`rm -rf ${nextjsTempDir}`, "Clean up next.js temp files");
+    await scaffoldNext({ path: sm.basePath, manager: sm.manager, chainId: sm.chainId }, undefined, sm);
+  } else if (appScaffold === "script") {
+    await scaffoldScript({ path: sm.basePath, manager: sm.manager, chainId: sm.chainId }, undefined, sm);
   } else if (appScaffold === "forge") {
-    // Delete app folder
-    await sm.rm("app", `  - Remove cloned quickstart scaffold's ${chalk.bold("app")} folder`);
+    await scaffoldForge({ path: sm.basePath, manager: sm.manager, chainId: sm.chainId }, undefined, sm);
+  } else {
+    throw new Error(`Invalid app scaffold choice: ${appScaffold}`);
   }
-
-  // Find and replace all
-  sm.findAndReplaceAll("Update chain data");
-
-  // Remove cloned repo
-  await sm.exec(`rm -rf ${tempDir}`, "Clean up build files");
-
-  // Move back to starting directory
-  process.chdir(startingDir);
 }
--- axiom-init/src/projectScaffold/script.ts
+++ axiom-init/src/projectScaffold/script.ts
@@ -1,4 +1,3 @@
-import path from 'path';
 import chalk from 'chalk';
 import prompt, { PromptObject } from 'prompts';
 import { ProjectScaffoldManager } from "./projectScaffoldManager"
@@ -44,7 +43,7 @@ export const scaffoldScript = async (
     let answers = await prompt(setupQuestions)
     
     if (answers.path === "") {
-      answers.path = "./app";
+      answers.path = "axiom-quickstart";
     }
 
     options = {
@@ -56,9 +55,6 @@ export const scaffoldScript = async (
     validatePackageManager(options.manager);
 
     sm = new ProjectScaffoldManager(options.path, options.manager, options.chainId);
-  } else {
-    // Set the ProjectScaffoldManager's path to the new path
-    sm.setPath(options.path);
   }
 
   const startingPath = process.cwd();
@@ -70,25 +66,44 @@ export const scaffoldScript = async (
   sm.mkdir(".", `  - Create directory ${chalk.bold(sm.basePath)}`);
 
   // Move to base path
-  process.chdir(sm.basePath);
+  sm.setPath(sm.basePath);
 
   // Clone the axiom-quickstart template
   console.log("Fetching Axiom quickstart template...");
   const tempDir = `.axiom-temp-${Date.now()}`; 
-  await sm.exec(`git clone --depth 1 https://github.com/axiom-crypto/axiom-quickstart.git ${tempDir}`, "Clone Axiom quickstart template");
+  await sm.execWithStream(`git clone --depth 1 https://github.com/axiom-crypto/axiom-quickstart.git ${tempDir}`, [], "Clone Axiom quickstart template");
+  sm.cp(`${tempDir}/.`, ".", `  - Copy files to ${chalk.bold(sm.basePath)}`);
 
-  const tempAppDir = path.join(tempDir, "app");
+  // Remove .git folder from cloned quickstart scaffold
+  await sm.rm(".git", `  - Remove .git folder from cloned quickstart scaffold`);
 
-  // Copy files to target path
-  sm.cp(`${tempAppDir}/.`, ".", `  - Copy files to ${chalk.bold(sm.basePath)}`);
+  // Remove lib folder from cloned quickstart scaffold
+  await sm.rm("lib", `  - Remove lib folder from cloned quickstart scaffold`);
+
+  // Initialize git repo
+  await sm.exec("git init", "Initialize git repository");
+
+  // Create an inital commit
+  await sm.execWithStream("git add .", [], "  - Add all files to git");
+  await sm.execWithStream("git commit -m 'Initial commit'", [], "  - Create initial commit");
+
+  // Install axiom-std
+  await sm.execWithStream("forge install axiom-crypto/axiom-std", [], "Install axiom-std");
 
   // Find and replace all
   sm.findAndReplaceAll("Update chain data");
 
+  // Install package dependencies
+  console.log("Installing package dependencies...");
+  await sm.execWithStream(sm.manager, [sm.installCmd], `Install package dependencies`);
+
   // Clean up cloned repo
   await sm.exec(`rm -rf ${tempDir}`, "Clean up build files");
 
-  // Move back to starting path
+  await sm.execWithStream("git add .", [], "  - Add updated chain data files to git");
+  await sm.execWithStream("git commit -m 'Update chain data'", [], "  - Update chain data commit");
+
+  // cd back to starting path
   process.chdir(startingPath);
 
   if (shouldPrint) {
